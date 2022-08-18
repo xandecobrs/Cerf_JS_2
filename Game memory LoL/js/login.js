55cb96a1af285b8bad53de8b4c91f6db06cca0bc
xandeco <alexandregs123@gmail.com>
2022-08-18 19:33:25 -0300

    elite commit

---
+++ Game memory LoL/js/login.js
@@ -0,0 +1,23 @@
+console.log('JS >>>>>>>> FUNCIONANDO')
+
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login_form')
+
+const validadeInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled')
+    } else {
+        button.setAttribute('disabled', '')
+    }
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem('player', input.value);
+    window.location = 'game.html'
+}
+
+input.addEventListener('input', validadeInput);
+form.addEventListener('submit', handleSubmit);
